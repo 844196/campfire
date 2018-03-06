3f4a6f417b4c4945275eaecafca118851512a38a
Masaya Takeda <844196@gmail.com>
2018-03-06 17:46:08 +0900

    setup common toast structure

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+md-snackbar(:md-position="position", :md-duration="duration", :md-active.sync="visible")
+  span {{ message }}
+  md-button.md-primary(@click="visible = false") Close
+</template>
+
+<script>
+import Vue from 'vue'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  name: 'Snackbar',
+  computed: {
+    ...mapState('snackbar', [
+      'message',
+      'position',
+      'duration'
+    ]),
+    visible: {
+      get () {
+        return this.$store.state.snackbar.visible
+      },
+      set (value) {
+        this.$store.commit('snackbar/changeVisibility', value)
+      }
+    }
+  }
+})
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from '@/store/auth'
 import memos from '@/store/memos'
+import snackbar from '@/store/snackbar'
 import { firebaseMutations } from 'vuexfire'
 
 Vue.use(Vuex)
@@ -9,7 +10,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     auth,
-    memos
+    memos,
+    snackbar
   },
   mutations: firebaseMutations
 })
