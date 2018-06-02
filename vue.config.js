d193855da3b41519a11f669c64aac9f0f43270e1
Masaya Takeda <844196@gmail.com>
2018-06-02 22:15:36 +0900

    本番ビルド時にソースマップを生成しないように

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   baseUrl: '/',
   configureWebpack (config) {
     config.performance = { hints: false }
+    config.devtool = process.env.NODE_ENV === 'production' ? false : 'cheap-module-eval-source-map'
   }
 }
